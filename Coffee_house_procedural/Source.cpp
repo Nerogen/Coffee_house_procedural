d8268431a203f448214cf479644163786732ba0b
Nero <14k21096@gmail.com>
2022-05-18 15:46:02 +0300

    corrected inaccuracies

--- Coffee_house_procedural/Source.cpp
+++ Coffee_house_procedural/Source.cpp
@@ -1,161 +1,154 @@
 #include <iostream>
-#include "windows.h"
 using namespace std;
 
 
-void coffee_machine(int& number_of_coffee, int time, int how_much_is_needed) {
-	Sleep(time * 1000); //this function counts the time in thousandths of a second
+void coffee_machine(int& number_of_coffee, int how_much_is_needed, int time_of_make, int& current_time) {
+	// make coffe from a few coffee beans and update time 
 	number_of_coffee -= 1;
+	current_time += time_of_make;
 }
 
-void cappuccinator(int& number_of_milk, int time, int how_much_is_needed) {
-	Sleep(time * 1000); //this function counts the time in thousandths of a second
+void cappuccinator(int& number_of_milk, int how_much_is_needed, int time_of_make, int& current_time) {
+	// whipping milk and update time 
 	number_of_milk -= 1;
+	current_time += time_of_make;
 }
 
 int random() {
+	// get new random number
 	srand(time(NULL));
 	return rand();
 }
 
-int cappuccino(int& number_of_coffee, int& number_of_milk, int& number_of_syrup, int time_of_make) {
-	//check if there are enough ingredients and return 1 if cooked, otherwise 0
+int cappuccino(int& number_of_coffee, int& number_of_milk, int& number_of_syrup, int time_of_make, int& current_time) {
+	// check if there are enough ingredients and return 1 if cooked, otherwise 0
 	if (number_of_coffee >= 2 && number_of_milk >= 2 && number_of_syrup >= 1) {
-		cappuccinator(number_of_milk, time_of_make, 2);
-		coffee_machine(number_of_coffee, time_of_make, 2);
+		cappuccinator(number_of_milk, 2, time_of_make, current_time);
+		coffee_machine(number_of_coffee, 2, time_of_make, current_time);
 		number_of_syrup -= 1;
 		return 1;
-	}
-	else {
+	} else {
 		return 0;
 	}
 }
 
-int late(int& number_of_coffee, int& number_of_milk, int time_of_make) {
-	//check if there are enough ingredients and return 1 if cooked, otherwise 0
+int late(int& number_of_coffee, int& number_of_milk, int time_of_make, int& current_time) {
+	// check if there are enough ingredients and return 1 if cooked, otherwise 0
 	if (number_of_coffee >= 1 && number_of_milk >= 1) {
-		cappuccinator(number_of_milk, time_of_make, 1);
-		coffee_machine(number_of_coffee, time_of_make, 1);
+		cappuccinator(number_of_milk, 1, time_of_make, current_time);
+		coffee_machine(number_of_coffee, 1, time_of_make, current_time);
 		return 1;
-	}
-	else {
+	} else {
 		return 0;
 	}
 }
 
-int espresso(int& number_of_coffee, int time_of_make) {
-	//check if there are enough ingredients and return 1 if cooked, otherwise 0
+int espresso(int& number_of_coffee, int time_of_make, int& current_time) {
+	// check if there are enough ingredients and return 1 if cooked, otherwise 0
 	if (number_of_coffee >= 5) {
-		coffee_machine(number_of_coffee, time_of_make, 5);
+		coffee_machine(number_of_coffee, 5, time_of_make, current_time);
 		return 1;
-	}
-	else {
+	} else {
 		return 0;
 	}
 }
 
-int dessert(int& number_of_desserts, int time_of_get) {
-	//check if there are enough desserts in stock and return 1 if there is, otherwise 0
+int dessert(int& number_of_desserts, int delivery_time, int &current_time) {
+	// check if there are enough desserts in stock and return 1 if there is, otherwise 0
 	if (number_of_desserts >= 1) {
-		Sleep(time_of_get * 1000);
 		number_of_desserts -= 1;
+		current_time += delivery_time;
 		return 1;
-	}
-	else {
+	} else {
 		return 0;
 	}
-
 }
 
-int order(int& queue, int& count_of_visitors, int& number_of_desserts, int& number_of_coffee_sids, int& number_of_milk_bottles, int& number_of_syrup, int& time_of_make) {
+int order(int& queue, int& count_of_visitors, int& number_of_desserts, int& number_of_coffee, int& number_of_milk_bottles, int& number_of_syrup, int& time_of_make, int& current_time) {
 	// function for service clients 
 	int rating = 0;
 	if (queue) {
-		int order = random() % 3 + 2;
 
-		if (order == 1) {
-			int value = dessert(number_of_desserts, time_of_make);
-			if (value == 1) {
+		int what_need_buy = random() % 3 + 2;
+
+		if (what_need_buy == 1) {
+			int made = dessert(number_of_desserts, time_of_make, current_time);
+			if (made == 1) {
 				rating += 5;
-			}
-			else {
+			} else {
 				rating += 1;
 			}
 		}
 
-		else if (order == 2) {
-			int value = cappuccino(number_of_coffee_sids, number_of_milk_bottles, number_of_syrup, time_of_make);
-			if (value == 1) {
+		else if (what_need_buy == 2) {
+			int made = cappuccino(number_of_coffee, number_of_milk_bottles, number_of_syrup, time_of_make, current_time);
+			if (made == 1) {
 				rating += 5;
-			}
-			else {
+				current_time += time_of_make;
+			} else {
 				rating += 1;
 			}
 		}
 
-		else if (order == 3) {
-			int value = late(number_of_coffee_sids, number_of_milk_bottles, time_of_make);
-			if (value == 1) {
+		else if (what_need_buy == 3) {
+			int made = late(number_of_coffee, number_of_milk_bottles, time_of_make, current_time);
+			if (made == 1) {
 				rating += 5;
-			}
-			else {
+				current_time += time_of_make;
+			} else {
 				rating += 1;
 			}
 		}
 
 		else {
-			int value = espresso(number_of_coffee_sids, time_of_make);
-			if (value == 1) {
+			int made = espresso(number_of_coffee, time_of_make, current_time);
+			if (made == 1) {
 				rating += 5;
-			}
-			else {
+				current_time += time_of_make;
+			} else {
 				rating += 1;
 			}
 		}
+
 		count_of_visitors += 1;
 		queue -= 1;
 
 	}
 
-	else {
-		//cout << "Empty" << endl;
-	}
-
 	return rating;
 }
 
-void coffee_house(int number_of_desserts, int number_of_coffee_sids, int number_of_milk_bottles, int number_of_syrup, int time_of_work, int time_of_visits, int time_of_make, int time_of_wait) {
+void coffee_house(int number_of_desserts, int number_of_coffee, int number_of_milk, int number_of_syrup, int time_of_work, int time_of_visits, int time_of_make, int time_of_wait) {
 	// model action
 	int rating = 0;
 	int count_of_visitors = 0;
-	int time_work = 1;
+	int current_time = 1;
 	int first_queue = 0;
 	int second_queue = 0;
-	while (time_of_work + 1 != time_work)
+
+	while (time_of_work >= current_time)
 	{
 		cout << "Number of visitors in first queue: " << first_queue << endl;
 		cout << "Number of visitors in second queue: " << second_queue << endl;
 		cout << "Rating: " << rating << endl;
 
-		// adding newly arrived visitors to the queue
+		// adding new arrived visitors to the queue
 		for (int i = 0; i < time_of_visits; i++) {
 			if ((random() + i) % 2 == 0) {
 				first_queue += 1;
-			}
-			else {
+			} else {
 				second_queue += 1;
 			}
 		}
 
-		//if the waiting time for the order has passed, the visitors who have not been served leave
-		if (time_of_wait % time_work == 0) {
+		// if the waiting time for the order has passed, the visitors who have not been served leave
+		if (time_of_wait % current_time == 0) {
 			for (int i = 0; i < time_of_visits; i++) {
 				if ((random() + i) % 2 == 0) {
 					if (first_queue > 0) {
 						first_queue -= 1;
 					}
-				}
-				else {
+				} else {
 					if (second_queue > 0) {
 						second_queue -= 1;
 					}
@@ -163,13 +156,14 @@ void coffee_house(int number_of_desserts, int number_of_coffee_sids, int number_
 			}
 		}
 
-
-		rating += order(first_queue, count_of_visitors, number_of_desserts, number_of_coffee_sids, number_of_milk_bottles, number_of_syrup, time_of_make);
-		rating += order(second_queue, count_of_visitors, number_of_desserts, number_of_coffee_sids, number_of_milk_bottles, number_of_syrup, time_of_make);
-		time_work += 1;
+		// two calls 'cause we have two queues
+		rating += order(first_queue, count_of_visitors, number_of_desserts, number_of_coffee, number_of_milk, number_of_syrup, time_of_make, current_time);
+		rating += order(second_queue, count_of_visitors, number_of_desserts, number_of_coffee, number_of_milk, number_of_syrup, time_of_make, current_time);
+		current_time += 1;
 	}
 
 	auto avarage_rating = rating / count_of_visitors;
+
 	cout << "Today rating is: " << avarage_rating << endl;
 }
 
@@ -177,7 +171,7 @@ void coffee_house(int number_of_desserts, int number_of_coffee_sids, int number_
 
 void main() {
 	cout << "Start !" << endl;
-	// parameters(number_of_desserts, number_of_coffee_sids, number_of_milk_bottlesnumber_of_syrup, time_of_work, time_of_visits, time_of_make, time_of_wait)
-	coffee_house(10, 10, 10, 10, 10, 10, 0.5, 3);
+	// parameters(number_of_desserts, number_of_coffee, number_of_milk, number_of_syrup, time_of_work, time_of_visits, time_of_make, time_of_wait)
+	coffee_house(10, 10, 10, 40, 10, 10, 0.5, 3);
 	cout << "Finish !" << endl;
 }
